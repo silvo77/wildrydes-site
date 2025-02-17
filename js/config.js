8510055573551eaada00cefb3e2057d4df4e8dae
silvo77 <silvo@tiscali.it>
2025-02-17 15:09:04 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ajrUwfJXz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1gdldu5f171bommtnmthnh362j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_3AJrXU0xl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '43a80rrk6pi1ulepv6e4eb42me', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
